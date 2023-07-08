1a8deadccbee73f987dfbc28601bf72e237517a0
Irving Rodriguez <irvingrodriguezromero@gmail.com>
2023-07-08 04:17:43 -0600

    [Fix]: Fix styles content

--- src/styled/common/main.styles.js
+++ src/styled/common/main.styles.js
@@ -22,7 +22,7 @@ export const Container = styled.main`
 
 export const Content = styled.div`
     width: 100%;
-    padding: 70px 20px 20px 20px;
+    padding: 80px 20px 20px 20px;
     display: flex;
     flex-direction: column;
     gap: 20px;
